52b40d0410778a68abd5ee0284c26fe6d89ad3b5
Nator Verinumbe <natverior1@gmail.com>
2020-12-20 09:00:33 +0100

    test code for jsonError not working

--- JsonError_test.go
+++ JsonError_test.go
@@ -1,15 +1,34 @@
-package jsonWrite_test
+package jsonWrite//_test
 
 import (
+	//"github.com/IamNator/mysql-golang-web/session"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestJsonError(t *testing.T) {
-	req, err := http.NewRequest("GET","/")
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(func (w http.ResponseWriter, _ * http.Request){
+		JsonError(w, "error", http.StatusOK)
+	})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	expected := `{ error: "error" }`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
 }
-
-func BenchmarkJsonError(b *testing.B) {
-
-}
+//
+//func BenchmarkJsonError(b *testing.B) {
+//
+//}
