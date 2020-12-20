adbd442a92834824316711c6592bd399e3ae2c91
Nator Verinumbe <natverior1@gmail.com>
2020-12-20 18:55:34 +0100

    handle json*.encode err

--- JsonError.go
+++ JsonError.go
@@ -5,15 +5,20 @@ import (
 	"net/http"
 )
 
-//Writes Error Message to ResponseWriter in the form a json
+//Writes Error Message to ResponseWriter in json format
 //Similar to http.Error(w ResponseWriter, error string, code int)
+//Example of json written is --> {"error":ErrorMessage}
 func Error(w http.ResponseWriter, ErrorMessage string, ErrorCode int) {
 	w.WriteHeader(ErrorCode)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(struct {
+	err := json.NewEncoder(w).Encode(struct {
 		Error string `json:"error"`
 	}{
 		Error: ErrorMessage,
 	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("unable to encode json response"))
+	}
 }
 
--- JsonResponse.go
+++ JsonResponse.go
@@ -5,14 +5,21 @@ import (
 	"net/http"
 )
 
+//Writes a Response Message to ResponseWriter in json format
+//Similar to http.Write(string)
+//Example of json written is --> {"title": Title, "message":Message}
 func Response(w http.ResponseWriter, Title string, Message string, StatusCode int) {
 	w.WriteHeader(StatusCode)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(struct {
+	err := json.NewEncoder(w).Encode(struct {
 		Title string  `json:"title"`
 		Message string `json:"message"`
 	}{
 		Title: Title,
 		Message: Message,
 	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("unable to encode json response"))
+	}
 }
